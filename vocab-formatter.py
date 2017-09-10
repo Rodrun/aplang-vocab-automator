81ae4be9da93cdde0c13f1ee6472bd610a07cce6
Juan Mendez <jmendg314@gmail.com>
2017-09-09 23:10:16 -0400

    Upload the only necessary file

---
+++ vocab-formatter.py
@@ -0,0 +1,31 @@
+# For AP Lang vocab lists, will return each word itself in the list.
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_definition(search):
+    """
+    Get the first definition of the word using merriam-webster dictionary online.
+    :param search: Word to find the definition of.
+    :return: Definition of search.
+    """
+    # Read HTML website
+    req = requests.get("https://merriam-webster.com/dictionary/" + search)
+    req.raise_for_status()
+    soup = BeautifulSoup(req.text, "html.parser")
+    elems = soup.select('p[class="definition-inner-item with-sense"] > span')
+    if len(elems) == 0:
+        elems = soup.select('p[class="definition-inner-item"] > span')
+    return elems[0]
+
+
+path = input("File: ")
+# Read and store into list
+with open(path) as f:
+    content = f.readlines()
+f.close()
+
+definition_list = []
+for element in content:
+    word = element[:element.find(":")]
+    print(word + "\t" + str.lstrip(get_definition(word).text).split(":")[1])
